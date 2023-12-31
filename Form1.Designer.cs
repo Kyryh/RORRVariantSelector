167bb6967247a3f55d9aaddbb369310596264cb9
Kyryh <kyrykiller07@gmail.com>
2023-12-31 19:03:53 +0100

    Added most of the gui i think

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,326 @@
         /// </summary>
         private void InitializeComponent()
         {
+            checkedListBox1 = new CheckedListBox();
+            checkedListBox2 = new CheckedListBox();
+            label2 = new Label();
+            label1 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            checkedListBox3 = new CheckedListBox();
+            checkedListBox4 = new CheckedListBox();
+            checkedListBox5 = new CheckedListBox();
+            checkedListBox6 = new CheckedListBox();
+            label10 = new Label();
+            checkedListBox7 = new CheckedListBox();
+            checkedListBox8 = new CheckedListBox();
+            checkedListBox9 = new CheckedListBox();
+            checkedListBox10 = new CheckedListBox();
+            openFileDialog1 = new OpenFileDialog();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
             SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.CheckOnClick = true;
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Items.AddRange(new object[] { "1", "2 (Artifact of Honor)", "3 (Artifact of Honor, Commando Prism)", "4 (Artifact of Cognation)", "5", "6 (Environment Log)" });
+            checkedListBox1.Location = new Point(9, 86);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(327, 112);
+            checkedListBox1.TabIndex = 0;
+            checkedListBox1.SelectedIndexChanged += checkedListBox1_SelectedIndexChanged;
+            // 
+            // checkedListBox2
+            // 
+            checkedListBox2.CheckOnClick = true;
+            checkedListBox2.FormattingEnabled = true;
+            checkedListBox2.Items.AddRange(new object[] { "1 (Artifact of Kin)", "2 (Artifact of Kin, Environment Log)", "3 (Artifact of Kin, Artificer Prism)", "4 ", "5", "6" });
+            checkedListBox2.Location = new Point(342, 86);
+            checkedListBox2.Name = "checkedListBox2";
+            checkedListBox2.Size = new Size(394, 112);
+            checkedListBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F);
+            label2.Location = new Point(342, 55);
+            label2.Name = "label2";
+            label2.Size = new Size(104, 28);
+            label2.TabIndex = 2;
+            label2.Text = "Dried Lake";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F);
+            label1.Location = new Point(9, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(147, 28);
+            label1.TabIndex = 3;
+            label1.Text = "Desolate Forest";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15F);
+            label3.Location = new Point(9, 201);
+            label3.Name = "label3";
+            label3.Size = new Size(138, 28);
+            label3.TabIndex = 4;
+            label3.Text = "Damp Caverns";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15F);
+            label4.Location = new Point(342, 201);
+            label4.Name = "label4";
+            label4.Size = new Size(124, 28);
+            label4.TabIndex = 5;
+            label4.Text = "Sky Meadow";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15F);
+            label5.Location = new Point(7, 347);
+            label5.Name = "label5";
+            label5.Size = new Size(134, 28);
+            label5.TabIndex = 6;
+            label5.Text = "Ancient Valley";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 15F);
+            label6.Location = new Point(346, 347);
+            label6.Name = "label6";
+            label6.Size = new Size(130, 28);
+            label6.TabIndex = 7;
+            label6.Text = "Sunken Tomb";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 15F);
+            label7.Location = new Point(9, 493);
+            label7.Name = "label7";
+            label7.Size = new Size(156, 28);
+            label7.TabIndex = 8;
+            label7.Text = "Magma Barracks";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 15F);
+            label8.Location = new Point(346, 493);
+            label8.Name = "label8";
+            label8.Size = new Size(116, 28);
+            label8.TabIndex = 9;
+            label8.Text = "Hive Cluster";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 15F);
+            label9.Location = new Point(9, 639);
+            label9.Name = "label9";
+            label9.Size = new Size(187, 28);
+            label9.TabIndex = 10;
+            label9.Text = "Temple of the Elders";
+            // 
+            // checkedListBox3
+            // 
+            checkedListBox3.CheckOnClick = true;
+            checkedListBox3.FormattingEnabled = true;
+            checkedListBox3.Items.AddRange(new object[] { "1", "2 (Artifact of Distortion)", "3", "4", "5", "6" });
+            checkedListBox3.Location = new Point(9, 232);
+            checkedListBox3.Name = "checkedListBox3";
+            checkedListBox3.Size = new Size(331, 112);
+            checkedListBox3.TabIndex = 11;
+            // 
+            // checkedListBox4
+            // 
+            checkedListBox4.CheckOnClick = true;
+            checkedListBox4.FormattingEnabled = true;
+            checkedListBox4.Items.AddRange(new object[] { "1", "2 (Artifact of Spite, Loader Prism)", "3", "4", "5", "6" });
+            checkedListBox4.Location = new Point(346, 232);
+            checkedListBox4.Name = "checkedListBox4";
+            checkedListBox4.Size = new Size(390, 112);
+            checkedListBox4.TabIndex = 12;
+            // 
+            // checkedListBox5
+            // 
+            checkedListBox5.CheckOnClick = true;
+            checkedListBox5.FormattingEnabled = true;
+            checkedListBox5.Items.AddRange(new object[] { "1 (Artifact of Glass)", "2 (Artifact of Glass, Boar Beach, Acrid Prism, Drifter Prism)", "3 (Artifact of Glass)", "4 (Environment Log)", "5", "6" });
+            checkedListBox5.Location = new Point(9, 378);
+            checkedListBox5.Name = "checkedListBox5";
+            checkedListBox5.Size = new Size(331, 112);
+            checkedListBox5.TabIndex = 13;
+            // 
+            // checkedListBox6
+            // 
+            checkedListBox6.CheckOnClick = true;
+            checkedListBox6.FormattingEnabled = true;
+            checkedListBox6.Items.AddRange(new object[] { "1 (Acrid)", "2 (Acrid)", "3 (Acrid)", "4", "5 (Artifact of Dissonance Shard, Environment Log)", "6" });
+            checkedListBox6.Location = new Point(346, 378);
+            checkedListBox6.Name = "checkedListBox6";
+            checkedListBox6.Size = new Size(390, 112);
+            checkedListBox6.TabIndex = 14;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 15F);
+            label10.Location = new Point(346, 639);
+            label10.Name = "label10";
+            label10.Size = new Size(113, 28);
+            label10.TabIndex = 15;
+            label10.Text = "Risk of Rain";
+            // 
+            // checkedListBox7
+            // 
+            checkedListBox7.CheckOnClick = true;
+            checkedListBox7.FormattingEnabled = true;
+            checkedListBox7.Items.AddRange(new object[] { "1 (Direseeker)", "2 (Direseeker, Huntress Prism)", "3", "4", "5", "6" });
+            checkedListBox7.Location = new Point(9, 524);
+            checkedListBox7.Name = "checkedListBox7";
+            checkedListBox7.Size = new Size(331, 112);
+            checkedListBox7.TabIndex = 16;
+            // 
+            // checkedListBox8
+            // 
+            checkedListBox8.CheckOnClick = true;
+            checkedListBox8.FormattingEnabled = true;
+            checkedListBox8.Items.AddRange(new object[] { "1 (Artifact of Command, Bloated Survivor)", "2 (Artifact of Command)", "3", "4", "5", "6" });
+            checkedListBox8.Location = new Point(346, 524);
+            checkedListBox8.Name = "checkedListBox8";
+            checkedListBox8.Size = new Size(390, 112);
+            checkedListBox8.TabIndex = 17;
+            // 
+            // checkedListBox9
+            // 
+            checkedListBox9.CheckOnClick = true;
+            checkedListBox9.FormattingEnabled = true;
+            checkedListBox9.Items.AddRange(new object[] { "1", "2", "3", "4", "5 (Environment Log)", "6" });
+            checkedListBox9.Location = new Point(9, 670);
+            checkedListBox9.Name = "checkedListBox9";
+            checkedListBox9.Size = new Size(331, 112);
+            checkedListBox9.TabIndex = 18;
+            // 
+            // checkedListBox10
+            // 
+            checkedListBox10.CheckOnClick = true;
+            checkedListBox10.FormattingEnabled = true;
+            checkedListBox10.Items.AddRange(new object[] { "1 (Environment Log, Artifact of Origin)", "2 (Environment Log, Artifact of Origin, Acrid Prism, Mercenary Prism)" });
+            checkedListBox10.Location = new Point(346, 670);
+            checkedListBox10.Name = "checkedListBox10";
+            checkedListBox10.Size = new Size(390, 112);
+            checkedListBox10.TabIndex = 19;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(9, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 23);
+            button1.TabIndex = 20;
+            button1.Text = "Load game files";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(115, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(134, 23);
+            button2.TabIndex = 21;
+            button2.Text = "Save variants selection";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(255, 12);
+            button3.Name = "button3";
+            button3.Size = new Size(129, 23);
+            button3.TabIndex = 22;
+            button3.Text = "Restore stage variants";
+            button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(748, 789);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(checkedListBox10);
+            Controls.Add(checkedListBox9);
+            Controls.Add(checkedListBox8);
+            Controls.Add(checkedListBox7);
+            Controls.Add(label10);
+            Controls.Add(checkedListBox6);
+            Controls.Add(checkedListBox5);
+            Controls.Add(checkedListBox4);
+            Controls.Add(checkedListBox3);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(checkedListBox2);
+            Controls.Add(checkedListBox1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "RORRVariantSelector";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private CheckedListBox checkedListBox1;
+        private CheckedListBox checkedListBox2;
+        private CheckedListBox checkedListBox3;
+        private CheckedListBox checkedListBox4;
+        private CheckedListBox checkedListBox5;
+        private CheckedListBox checkedListBox6;
+        private Label label10;
+        private CheckedListBox checkedListBox7;
+        private CheckedListBox checkedListBox8;
+        private CheckedListBox checkedListBox9;
+        private CheckedListBox checkedListBox10;
+        private OpenFileDialog openFileDialog1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
+
+
 }
