4bcbda3f22dc63b7a65a9a9d2919e07899ae595b
Kyryh <kyrykiller07@gmail.com>
2024-01-01 14:26:36 +0100

    Added imports

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace RORRVariantSelector
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace RORRVariantSelector
 {
     partial class Form1
     {
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,16 @@
 #pragma warning disable CS8622
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+
 namespace RORRVariantSelector
 {
     public partial class Form1 : Form {
