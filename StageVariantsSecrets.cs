cef2acc1fd9fcf16aadc20352b41076e81335bd9
Kyryh <kyrykiller07@gmail.com>
2024-01-01 10:55:16 +0100

    Formatting and removing unused imports

--- StageVariantsSecrets.cs
+++ StageVariantsSecrets.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RORRVariantSelector {
+﻿namespace RORRVariantSelector {
     internal static class StageVariantsSecrets {
+
+
         public static string[][] desolateForest = [
             [
                 
